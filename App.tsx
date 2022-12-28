5104f5d8f23ee3cddf6112e665762700c0228410
Oscar Andres Rosas Hernandez <soyoscarrh@gmail.com>
2022-12-27 18:41:54 -0600

    feat(font): add SF Pro Display as the app font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components/native';
 import {Provider} from 'react-redux';
 import {persistor, store} from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import {loadAsync} from 'expo-font';
+
 import Challenge from './src';
 
 export default function App() {
+  const [attempedToLoadFont, setAttempedToLoadFont] = useState(false);
+  useEffect(() => {
+    const font = {'SF Pro Display': require('./assets/fonts/sf_pro_display.ttf')} as const;
+    loadAsync(font)
+      .finally(() => setAttempedToLoadFont(true));
+  });
+
+  if (!attempedToLoadFont) {
+    return null;
+  }
+  
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
